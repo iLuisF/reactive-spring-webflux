03016e9a5032ecce978c5c3d8131fddff7a1d4f7
Luis Flores <24851416+iLuisF@users.noreply.github.com>
2023-02-01 10:11:20 -0600

    spring webflux endpoint test with approach different

--- movies-info-service/src/test/java/unit/com/reactivespring/controller/FluxAndMonoControllerTest.java
+++ movies-info-service/src/test/java/unit/com/reactivespring/controller/FluxAndMonoControllerTest.java
@@ -1,19 +1,22 @@
 package com.reactivespring.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+import java.util.Objects;
 
 @AutoConfigureWebTestClient
 @WebFluxTest(controllers = FluxAndMonoController.class)
 class FluxAndMonoControllerTest {
 
-    private final WebTestClient webTestClient;
-
-    FluxAndMonoControllerTest(WebTestClient webTestClient) {
-        this.webTestClient = webTestClient;
-    }
+    @Autowired
+    private WebTestClient webTestClient;
 
     @Test
     void flux() {
@@ -25,4 +28,35 @@ class FluxAndMonoControllerTest {
                 .is2xxSuccessful()
                 .expectBodyList(Integer.class);
     }
+
+    @Test
+    void fluxApproach2() {
+        Flux<Integer> flux = webTestClient
+                .get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .returnResult(Integer.class)
+                .getResponseBody();
+        StepVerifier.create(flux)
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+
+    @Test
+    void fluxApproach3() {
+        webTestClient
+                .get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBodyList(Integer.class)
+                .consumeWith(listEntityExchangeResult -> {
+                    List<Integer> integers = listEntityExchangeResult.getResponseBody();
+                    assert (Objects.requireNonNull(integers).size() == 4);
+                });
+    }
+
 }
