ba8bb6f7954242ff1d04fbe313ec776fb8da19a3
Luis Flores <24851416+iLuisF@users.noreply.github.com>
2023-02-03 18:50:34 -0600

    feat: save a movie in reactive mongo

---
+++ movies-info-service/src/main/java/com/reactivespring/controller/MoviesInfoController.java
@@ -0,0 +1,26 @@
+package com.reactivespring.controller;
+
+import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.service.MovieInfoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/v1")
+public class MoviesInfoController {
+
+    private final MovieInfoService movieInfoService;
+
+    public MoviesInfoController(MovieInfoService movieInfoService) {
+        this.movieInfoService = movieInfoService;
+    }
+
+    @PostMapping("/movie-info")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<MovieInfo> addMovie(@RequestBody MovieInfo movie) {
+        System.out.println("addMovie");
+        return this.movieInfoService.addMovie(movie).log();
+    }
+
+}
---
+++ movies-info-service/src/main/java/com/reactivespring/service/MovieInfoService.java
@@ -0,0 +1,20 @@
+package com.reactivespring.service;
+
+import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.repository.MovieInfoRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class MovieInfoService {
+
+    private MovieInfoRepository repository;
+
+    public MovieInfoService(MovieInfoRepository repository) {
+        this.repository = repository;
+    }
+
+    public Mono<MovieInfo> addMovie(MovieInfo movie) {
+        return this.repository.save(movie);
+    }
+}
