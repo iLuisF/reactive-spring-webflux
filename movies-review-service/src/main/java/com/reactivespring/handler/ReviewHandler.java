546c68946bd92efd238865a02aac75c271bd6b94
Luis Flores <24851416+iLuisF@users.noreply.github.com>
2023-02-07 18:24:18 -0600

    feat: add movie and integration test

---
+++ movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
@@ -0,0 +1,26 @@
+package com.reactivespring.handler;
+
+import com.reactivespring.domain.Review;
+import com.reactivespring.repository.ReviewReactiveRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+public class ReviewHandler {
+
+    private final ReviewReactiveRepository repository;
+
+    public ReviewHandler(ReviewReactiveRepository repository) {
+        this.repository = repository;
+    }
+
+    public Mono<ServerResponse> add(ServerRequest request) {
+        return request.bodyToMono(Review.class)
+                .flatMap(repository::save)
+                .flatMap(ServerResponse.status(HttpStatus.CREATED)::bodyValue);
+    }
+
+}
--- movies-review-service/src/test/java/intg/com/reactivespring/routes/ReviewsIntgTest.java
+++ movies-review-service/src/test/java/intg/com/reactivespring/routes/ReviewsIntgTest.java
@@ -1,4 +1,68 @@
 package com.reactivespring.routes;
 
+import com.reactivespring.domain.Review;
+import com.reactivespring.repository.ReviewReactiveRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
 public class ReviewsIntgTest {
+
+    @Autowired
+    private WebTestClient client;
+    @Autowired
+    private ReviewReactiveRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        List<Review> reviewsList = List.of(
+                new Review(null, 1L, "Awesome Movie", 9.0),
+                new Review(null, 1L, "Awesome Movie1", 9.0),
+                new Review(null, 2L, "Excellent Movie", 8.0));
+        repository.saveAll(reviewsList).blockLast();
+    }
+
+    @AfterEach
+    void tearDown() {
+        repository.deleteAll().block();
+    }
+
+    @Test
+    void name() {
+        client
+                .get()
+                .uri("/v1/helloworld")
+                .exchange()
+                .expectBody(String.class)
+                .isEqualTo("Hello World");
+    }
+
+    @Test
+    void addReview() {
+        var review = new Review(null, 1L, "Awesome Movie", 9.0);
+        client
+                .post()
+                .uri("/v1/reviews")
+                .bodyValue(review)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(Review.class)
+                .consumeWith(reviewResponse -> {
+                    Review savedReview = reviewResponse.getResponseBody();
+                    assert savedReview != null;
+                    assertNotNull(savedReview.getReviewId());
+                });
+    }
 }
