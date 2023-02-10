3189592abe537e271064f75916f3c70590eb6356
Luis Flores <24851416+iLuisF@users.noreply.github.com>
2023-02-09 22:59:56 -0600

    unit test for add review endpoint

--- movies-review-service/src/test/java/unit/com/reactivespring/routes/ReviewsUnitTest.java
+++ movies-review-service/src/test/java/unit/com/reactivespring/routes/ReviewsUnitTest.java
@@ -1,4 +1,49 @@
 package com.reactivespring.routes;
 
+import com.reactivespring.domain.Review;
+import com.reactivespring.handler.ReviewHandler;
+import com.reactivespring.repository.ReviewReactiveRepository;
+import com.reactivespring.router.ReviewRouter;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.when;
+
+@WebFluxTest
+@ContextConfiguration(classes = {ReviewRouter.class, ReviewHandler.class})
+@AutoConfigureWebTestClient
 public class ReviewsUnitTest {
+
+    @MockBean
+    private ReviewReactiveRepository repository;
+    @Autowired
+    private WebTestClient client;
+    static String REVIEWS_URL = "/v1/reviews";
+
+    @Test
+    void addReview() {
+        var review = new Review(null, 1L, "Awesome Movie", 9.0);
+        when(repository.save(isA(Review.class)))
+                .thenReturn(Mono.just(new Review("abc", 1L, "Awesome Movie", 9.0)));
+        client
+                .post()
+                .uri(REVIEWS_URL)
+                .bodyValue(review)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(Review.class)
+                .consumeWith(reviewResponse -> {
+                    Review savedReview = reviewResponse.getResponseBody();
+                    assert savedReview != null;
+                    assertNotNull(savedReview.getReviewId());
+                });
+    }
 }
