b49a152e0dc9906f707defb5b084c395abc662d6
Luis Flores <24851416+iLuisF@users.noreply.github.com>
2023-02-11 13:09:43 -0600

    feat: Handling 4XX in MoviesInfoService in WebClient

---
+++ movies-service/src/main/java/com/reactivespring/exception/GlobalErrorHandler.java
@@ -0,0 +1,18 @@
+package com.reactivespring.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalErrorHandler {
+
+    @ExceptionHandler(MoviesInfoClientException.class)
+    public ResponseEntity<String> handleClientException(MoviesInfoClientException exception) {
+        log.error("Exception caught in handleClientException : {}", exception.getMessage());
+        return ResponseEntity.status(exception.getStatusCode()).body(exception.getMessage());
+    }
+
+}
