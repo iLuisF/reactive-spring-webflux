63c62338e8e86797ffba48dee755cb13fa7f3400
Luis Flores <24851416+iLuisF@users.noreply.github.com>
2023-02-11 19:06:32 -0600

    feat: Stubs for MoviesInfoService and ReviewService

--- movies-service/src/test/java/intg/com/reactivespring/controller/MoviesControllerIntgTest.java
+++ movies-service/src/test/java/intg/com/reactivespring/controller/MoviesControllerIntgTest.java
@@ -1,10 +1,19 @@
 package com.reactivespring.controller;
 
+import com.reactivespring.domain.Movie;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.Objects;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ActiveProfiles("test")
 @AutoConfigureWireMock(port = 8084)
@@ -18,5 +27,30 @@ import org.springframework.test.context.TestPropertySource;
 )
 public class MoviesControllerIntgTest {
 
+    @Autowired
+    private WebTestClient client;
 
+    @Test
+    void retrieveMovieById() {
+        String movieId = "abc";
+        stubFor(get(urlEqualTo("/v1/movie-info/" + movieId))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBodyFile("movieinfo.json")));
+        stubFor(get(urlPathEqualTo("/v1/reviews"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBodyFile("reviews.json")));
+        client
+                .get()
+                .uri("/v1/movies/{id}", movieId)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Movie.class)
+                .consumeWith(movieEntity -> {
+                    Movie movie = movieEntity.getResponseBody();
+                    assert Objects.requireNonNull(movie).getReviewList().size() == 2;
+                    assertEquals("Batman Begins", movie.getMovieInfo().getName());
+                });
+    }
 }
